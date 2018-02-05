e972933158dbc8d4c90dd511e5dfc8b7888f10fe
lonixoon <soinov.alexey@yandex.ru>
2018-02-05 17:59:38 +0700

    создал тикет и заполнил его данными

--- app/Athena.php
+++ app/Athena.php
@@ -20,22 +20,46 @@ class Athena extends Model
 
     /*
      * Формируем ссылки
+     * Получаем сессию первой переменной, второй ссылку на новый тикет и его ИД
+     * Возвращаем готовые ссылки
      */
-    public function crateLink($session)
+    public function crateLink($session, $urlCreateTiketAndId)
     {
         // сессия
-        $PHPSESSID = $session['PHPSESSID'];
+        $sessionId = $session['PHPSESSID'];
         // время жизни сессии
-        $internalurltime = $session['internalurltime'];
+        $sessionTime = $session['internalurltime'];
+        // ИД тикета
+        $idTiket = $urlCreateTiketAndId['idTiket'];
+        // Ссылка на новый тикет
+        $urlCreateTiket = $urlCreateTiketAndId['urlCreateTiket'];
 
 
         $link = [
             // сслыка для логаута, чтобы не засорять сессии
-            'urlLogout' => 'https://athena.easyvista.com/index.php?logout=1&PHPSESSID=' . $PHPSESSID
-                . '&internalurltime' . $internalurltime,
+            'urlLogout' => 'https://athena.easyvista.com/index.php?logout=1&PHPSESSID=' . $sessionId
+                . '&internalurltime' . $sessionTime,
+
             // создаём ссылку получения уникальной ссылки на новый тикет
-            'urlGetIdTiket' => 'index.php?PHPSESSID=' . $PHPSESSID . '&internalurltime=' . $internalurltime
+            'urlGetIdTiket' => 'index.php?PHPSESSID=' . $sessionId . '&internalurltime=' . $sessionTime
                 . '&theme=%7BC9F11D78-448E-4EAC-A766-C904ED98458E%7D&do_quickcall=2&quickcall_id=284152&new_rqst=1',
+
+            // ссылка на создание нового тикета
+            'urlCreateTiket' => $urlCreateTiket,
+
+            // ссылка для добавления данных в тикет
+            'urlFillingTiketForm' => 'iframefiche.php?PHPSESSID=' . $sessionId . '&internalurltime=' . $sessionTime
+                . '&theme=%7BC9F11D78-448E-4EAC-A766-C904ED98458E%7D&eventName=ShowEditGrid&q2_dialogId=%7B90D54E77-40A5-4F0F-8029-5A7A05F5814D%7D&sender=%7B98C92B3D-9EEF-4459-B6CD-7CCDB9ADED9D%7D&q2_section=1&q2_element=0&q2_id=' .
+                $idTiket . '&action_select=%7BE1FBEB97-20F1-4A05-AF92-7352695AFFB5%7D',
+
+            // ИД тикета
+            'idTiket' => $idTiket,
+
+            // ссылка для обновления данных формы (применение изменений данных формы)
+            'urlReloadForm' => 'reload_qc.php?PHPSESSID=' . $sessionId . '&internalurltime=' . $sessionTime
+            . '&theme=%7BC9F11D78-448E-4EAC-A766-C904ED98458E%7D&eventName=ShowEditGrid&q2_dialogId=%7B90D54E77-40A5-4F0F-8029-5A7A05F5814D%7D&sender=%7B98C92B3D-9EEF-4459-B6CD-7CCDB9ADED9D%7D&q2_section=0&q2_element=0&q2_id='
+            . $idTiket . '&field_to_reload=',
+
         ];
 
         return $link;
--- app/Http/Controllers/AthenaController.php
+++ app/Http/Controllers/AthenaController.php
@@ -12,35 +12,65 @@ use GuzzleHttp\Client;
 class AthenaController extends Controller
 {
     /*
-     * Главная упавляющаая функция, получает данные Портал Админ Классика в виде массива Витрина => Статус
+     * Главная упавляющаая функция
      */
     public function index()
     {
         $Athena = new Athena();
 
-        // получаем сессию и куки
+        // получаем сессию и куки из html
         $session = $this->getAuth();
         // формируем ссылки
-        $link = $Athena->crateLink($session);
-        // создаём тикет
-        $createTiket = $this->createTiket($session, $link);
+        $link = $Athena->crateLink($session, null);
+        // получаем ссылку на новый тикет и ИД этого тикета
+        $arrUrlCreateTiketAndIdTiket = $this->getNewTiketAndId($session, $link);
+        // снова формируем ссылки
+        $link = $Athena->crateLink($session, $arrUrlCreateTiketAndIdTiket);
 
-        return $createTiket;
+        // вставляем данные в верхнюю форму
+        $this->headerFormPost($session, $link);
+        // обновляем (сохраняем) заголовки верхней формы
+        $this->reloadFormGet($session, $link, 'NEWS_QUERY');
+        // вставляем данные в тело (нижнюю форму)
+        $this->bodyFormPost($session, $link);
+        // обновляем(сохраняем) приоритет тикета
+        $this->reloadFormGet($session, $link, 'URGENCY_ID');
+        // обновляем(сохраняем)
+        $this->reloadFormGet($session, $link, 'TITLE_EN');
+        // обновляем(сохраняем) топик
+        $this->reloadFormGet($session, $link, 'SD_CATALOG_ID');
+        // создаём тикет
+        $createTiketHtml = $this->createTiket($session, $link);
+
+
+        // завершаем сессию
+        $this->logout($session, $link);
+//        dd($reloadForm);
+        return $createTiketHtml;
     }
 
     /*
-     * Авторизация и получение всего HTML Портала Админ на нужной стринице
+     * Возвращает параметры для клиента
      */
-    public function getAuth()
+    public function client()
     {
-        $user = $this->userXruAuthData();
-
-        $Athena = new Athena();
-
         $client = new Client([
             'base_uri' => 'https://athena.easyvista.com/',
             'timeout' => 60.0,
         ]);
+        return $client;
+    }
+
+    /*
+     * Авторизация и получение всего HTML
+     * Возвращает сессию
+     */
+    public function getAuth()
+    {
+        $user = $this->userXruAuthData();
+        $Athena = new Athena();
+        $client = $this->client();
+
         $session['cookieJar'] = new CookieJar();
 
         $htmlAfterLogin = $client->request('POST', 'index.php', [
@@ -64,25 +94,154 @@ class AthenaController extends Controller
         return $session;
     }
 
-    public function createTiket($session, $link)
+
+    /*
+     * Принемает сессию и ссылки
+     * Возвращает ИД нового тикета и сслку на его создание
+     */
+    public function getNewTiketAndId($session, $link)
     {
-        $client = new Client([
-            'base_uri' => 'https://athena.easyvista.com/',
-            'timeout' => 60.0,
-        ]);
+        $client = $this->client();
+
         // получаем уникальный ИД для нового тикета
         $htmlGetIdTiket = $client->request('GET', $link['urlGetIdTiket'], ['cookies' => $session['cookieJar']])->getBody()->getContents();
 
         // получаем ссылку на новый тикет
         $arrCreateTiket = explode('href="/', $htmlGetIdTiket)[1];
-        $urlCreateTiket = explode('";', $arrCreateTiket)[0];
+        $urlCreateTiketAndId['urlCreateTiket'] = explode('";', $arrCreateTiket)[0];
+
+        // получаем ИД тикета
+        $urlCreateTiketAndId['idTiket'] = explode('&q2_id=', $urlCreateTiketAndId['urlCreateTiket'])[1];
+
+        return $urlCreateTiketAndId;
+    }
+
+    /*
+     * Принемает сессию и ссылки
+     * Возвращает новый тикет
+     */
+    public function createTiket($session, $link)
+    {
+        $client = $this->client();
 
         // получаем html созданного тикета
-        $htmlCreateTiket = $client->request('GET', $urlCreateTiket, ['cookies' => $session['cookieJar']])->getBody()->getContents();
-
-        // завершаем работу
-        $client->request('GET', $link['urlLogout'], ['cookies' => $session['cookieJar']]);
+        $htmlCreateTiket = $client->request('GET', $link['urlCreateTiket'], ['cookies' => $session['cookieJar']])->getBody()->getContents();
+//        $htmlCreateTiket = $client->request('GET', $link['urlCreateTiket'], ['cookies' => $session['cookieJar']]);
 
         return $htmlCreateTiket;
+    }
+
+
+    /*
+     *  Заполняем шапку формы отправки тикета
+     */
+    public function headerFormPost($session, $link)
+    {
+        $sit_id = '3644';
+        $sit = '016 - Auchan Novosibirsk';
+
+        $client = $this->client();
+        $headerForm = $client->request('POST', $link['urlFillingTiketForm'], [
+                'form_params' => [
+                    'PHPSESSID' => $session['PHPSESSID'],
+                    'updatedata' => 1,
+                    'AM_REQUESTOR.LAST_NAME' => 'FromAthenaInFify', // названию юзера в атене
+                    'pk_AM_REQUESTOR.LAST_NAME' => 'SD_REQUEST.REQUESTOR_ID',
+
+                    'AM_RECIPIENT.LAST_NAME' => 'FromAthenaInFify', // названию юзера в атене
+                    'pk_AM_RECIPIENT.LAST_NAME' => 'SD_REQUEST.RECIPIENT_ID',
+
+                    'SD_REQUEST_ORIGIN.NAME_EN' => 1,
+                    'pk_SD_REQUEST_ORIGIN.NAME_EN' => 'SD_REQUEST.REQUEST_ORIGIN_ID',
+                    'type_SD_REQUEST_ORIGIN.NAME_EN' => 'pk',
+                    '__GOTO_DLG_AM_LOCATION.LOCATION_EN' => $sit_id,
+                    '__WORLD_LINK_AM_LOCATION.LOCATION_EN_COUNTRY' => 'RUS',
+                    'AM_LOCATION.LOCATION_EN_hide' => $sit_id,
+                    '__POPUP_AM_LOCATION.LOCATION_EN' => $sit_id,
+                    'type_SD_REQUEST.PLANNED_CHANGE_DATE_END' => 'datetime',
+                    'type_SD_REQUEST.REQUESTED_CHANGE_DATE_END' => 'datetime',
+                    'timezone_SD_REQUEST.SUBMIT_DATE_UT' => 'Mjg0MTUy',
+
+                    'SD_IMPACT.IMPACT_EN' => '3 - Low', // из топика
+                    'pk_SD_IMPACT.IMPACT_EN' => '',
+                    'type_SD_IMPACT.IMPACT_EN' => 'string',
+
+                    'SD_SLA.NAME_EN' => '4 days',
+                    'pk_SD_SLA.NAME_EN' => '',
+
+                    'maintable' => 'SD_REQUEST',
+                    'AM_LOCATION.LOCATION_EN' => 'RUSSIA/STORE/' . $sit, // сит должен браться из таблицы магазин
+                    'pk_AM_LOCATION.LOCATION_EN' => 'SD_REQUEST.LOCATION_ID',
+                    'SD_REQUEST.AVAILABLE_FIELD_1' => '(AURA)',
+                    'OLD_SD_REQUEST.AVAILABLE_FIELD_1' => '',
+                    'pk_SD_REQUEST.AVAILABLE_FIELD_1' => 'SD_REQUEST.AVAILABLE_FIELD_1',
+                    'type_SD_REQUEST.AVAILABLE_FIELD_1' => 'string',
+                    'SD_REQUEST.AVAILABLE_FIELD_3' => 'AUUUUURAAA',
+                    'OLD_SD_REQUEST.AVAILABLE_FIELD_3' => '',
+                    'pk_SD_REQUEST.AVAILABLE_FIELD_3' => '',
+                    'type_SD_REQUEST.AVAILABLE_FIELD_3' => 'string',
+
+                    'editFormrequired_fields' => 'AM_REQUESTOR.LAST_NAME,AM_RECIPIENT.LAST_NAME,SD_REQUEST_ORIGIN.NAME_EN,AM_LOCATION.LOCATION_EN,SD_REQUEST.AVAILABLE_FIELD_1',
+                    'editFormdatetime_fields' => 'SD_REQUEST.REQUESTED_CHANGE_DATE_END,SD_REQUEST.PLANNED_CHANGE_DATE_END'
+                ],
+                'cookies' => $session['cookieJar'],
+            ]
+        );
+
+        return $headerForm;
+    }
+
+    public function bodyFormPost($session, $link)
+    {
+        $cim = '2';
+        $type = 'Incident';
+        $topic['name'] = 'COUNTRY BUSINESS/GIMA';
+        $set_topic = '181527';
+        $sit_id = '3644';
+        $text = 'lordfsghdsghdsfhdgf h dgf hdgf hdfg hgf';
+//        dd($link['idTiket']);
+
+
+        $client = $this->client();
+        $client->request('POST', $link['urlFillingTiketForm'], [
+                'form_params' => [
+                    'PHPSESSID' => $session['PHPSESSID'],
+                    'updatedata' => 4,
+                    'SD_URGENCY.URGENCY_EN' => $cim, //  cim из aura
+                    'pk_SD_URGENCY.URGENCY_EN' => 'SD_REQUEST.URGENCY_ID',
+                    'type_SD_URGENCY.URGENCY_EN' => 'pk',
+                    'SD_CATALOG.TITLE_EN' => $type . '/PROD/RUSSIA/' . $topic['name'], // тип .'/PROD/RUSSIA/'. название топика (полное соответствие  в aura)
+                    'SD_CATALOG.TITLE_EN_hide' => $set_topic,  // поле, отвечающее за каталог
+                    'pk_SD_CATALOG.TITLE_EN' => 'SD_REQUEST.SD_CATALOG_ID',
+                    'incident_location' => $sit_id,
+                    'SD_REQUEST.COMMENT' => $text,
+                    'OLD_SD_REQUEST.COMMENT' => '',
+                    'type_SD_REQUEST.COMMENT' => 'memo',
+                    'SD_REQUEST.DESCRIPTION' => '',
+                    'OLD_SD_REQUEST.DESCRIPTION' => '',
+                    'type_SD_REQUEST.DESCRIPTION' => 'memo',
+                    'maintable' => '',
+                    'maintable2' => 'SD_REQUEST',
+                    'maintable2_npk' => 1,
+                    'maintable2_pk0' => 'REQUEST_ID',
+                    'maintable2_pk_value0' => $link['idTiket'],
+                    'editForm2required_fields' => 'SD_CATALOG.TITLE_EN,SD_URGENCY.URGENCY_EN',
+                ],
+                'cookies' => $session['cookieJar'],
+            ]
+        );
+    }
+
+    public function reloadFormGet($session, $link, $text)
+    {
+        $client = $this->client();
+        $client->request('GET', $link['urlReloadForm'] . $text, ['cookies' => $session['cookieJar']]);
+    }
+
+    public function logout($session, $link)
+    {
+        $client = $this->client();
+        // завершаем работу
+        $client->request('GET', $link['urlLogout'], ['cookies' => $session['cookieJar']]);
     }
 }
